de9b9ef685b43d2c4a5464284b92848415f63c49
Jeff Shih <jeffshih@outlook.com>
2020-03-28 18:03:17 -0700

    Resolves consistency issue with auto exec.

--- popup.js
+++ popup.js
@@ -1,16 +1,18 @@
 (() => {
 	chrome.tabs.query({ active: true, currentWindow: true }, tab => {
 		const currentPage = tab[0].url;
-		const efmrlContainer = document.getElementById("efmrl-container");
+		const efmrlContainer = document.getElementById('efmrl-container');
 
-		efmrlContainer.innerHTML = `<iframe id="efmrl-portal" src="https://efmrl.link/" height="440" width="280"></iframe>`;
+		efmrlContainer.innerHTML = `<iframe id="efmrl-portal" src="https://www.efmrl.link/" height="440" width="280"></iframe>`;
 
-		const efmrlPortalEl = document.getElementById("efmrl-portal");
+		const efmrlPortalEl = document.getElementById('efmrl-portal');
 		const efmrlPortal = efmrlPortalEl.contentWindow;
 
-		efmrlPortalEl.addEventListener("load", () => {
-			efmrlPortalEl.focus();
-			efmrlPortal.postMessage(currentPage, "*");
+		efmrlPortalEl.addEventListener('load', () => {
+			setTimeout(() => {
+				efmrlPortalEl.focus();
+				efmrlPortal.postMessage(currentPage, '*');
+			}, 500);
 		});
 	});
 })();
