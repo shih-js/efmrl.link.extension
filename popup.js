26a125540d503e38769994de0cf7056469d5720a
Jeff Shih <jeffshih@outlook.com>
2020-06-16 14:06:50 -0700

    Update package.

--- popup.js
+++ popup.js
@@ -2,17 +2,32 @@
 	chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
 		const currentPage = tab[0].url;
 		const efmrlContainer = document.getElementById('efmrl-container');
-
-		efmrlContainer.innerHTML = `<iframe id="efmrl-portal" src="https://www.efmrl.link/" height="440" width="280" scrolling="no"></iframe>`;
+		efmrlContainer.innerHTML = `
+			<iframe
+				id="efmrl-portal"
+				title="efmrl"
+				src="https://www.efmrl.link/"
+				height="440"
+				width="280"
+				scrolling="no"
+				allow="layout-animations '*';
+					unoptimized-images '*';
+					oversized-images '*';
+					sync-script '*';
+					sync-xhr '*';
+					unsized-media '*';"
+			>
+			</iframe>`;
 
 		const efmrlPortalEl = document.getElementById('efmrl-portal');
-		const efmrlPortal = efmrlPortalEl.contentWindow;
 
 		efmrlPortalEl.addEventListener('load', () => {
+			console.log('currentPage: ', currentPage);
 			setTimeout(() => {
+				const efmrlPortal = efmrlPortalEl.contentWindow;
 				efmrlPortalEl.focus();
-				// efmrlPortal.postMessage(currentPage, '*');
-				efmrlPortal.postMessage({ type: 'copy-to-clipboard', currentPage }, '*');
+				efmrlPortal.postMessage(currentPage, '*');
+				// efmrlPortal.postMessage({ type: 'copy-to-clipboard', currentPage }, '*');
 			}, 500);
 		});
 	});
